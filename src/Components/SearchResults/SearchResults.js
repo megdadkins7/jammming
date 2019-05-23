fcff5b6a38346705af6409c4fd9fd9fea0e9ca64
Meghan Adkins <meghandadkins@gmail.com>
2019-05-23 09:12:22 -0400

    Final Capstone App

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -3,14 +3,18 @@ import './SearchResults.css';
 import TrackList from '../TrackList/TrackList';
 
 class SearchResults extends React.Component {
-    render() {
-        return (
-            <div className="SearchResults">
-                <h2>Results</h2>
-                <TrackList onAdd={this.props.onAdd} tracks={this.props.searchResults} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        <TrackList
+          tracks={this.props.searchResults}
+          onAdd={this.props.onAdd}
+          isRemoval={false}
+        />
+      </div>
+    );
+  }
 }
 
 export default SearchResults;
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import './TrackList.css';
-
 import Track from '../Track/Track';
 
 class TrackList extends React.Component {
-    render() {
-      return (
-        <div className="TrackList">
-            {this.props.tracks.map(track => <Track key={track.id} track={track}
+  render() {
+    return (
+      <div className="TrackList">
+        {this.props.tracks.map(track => {
+          return (
+            <Track
+              key={track.id}
+              track={track}
               onAdd={this.props.onAdd}
+              isRemoval={this.props.isRemoval}
               onRemove={this.props.onRemove}
-              />)}
-        </div>
-      );
-    }
+            />
+          );
+        })}
+      </div>
+    );
   }
-
+}
 
 export default TrackList;
